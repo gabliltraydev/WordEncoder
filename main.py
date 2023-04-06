915f27c3ae89e00dc94d88be1ed8491a68ee8150
gabliltraydev <99366409+gabliltraydev@users.noreply.github.com>
2023-04-06 19:33:16 +0200

    oui

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import random
 
 i = 0
+
 while i<26:
     rand = random.randint(1, 9999)
     print(rand)
     randStr = str(rand)
-    with open('decodeBase.txt', 'w') as file:
+    with open(str(i) + '.txt', 'w') as file:
         file.write(randStr)
     i = i+1
+
+
+
